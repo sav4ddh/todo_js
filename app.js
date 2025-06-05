d68d34c8374c88b4a6e15a90cec86bb7e5cb7abd
Muhammed Savaad <savaad.code@gmail.com>
2025-06-05 19:40:12 +0530

    Added localstorage, implemented search functionalities, corrected form and button mistakes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("pepcoTodos")) || [];
 let todaysTodos = [];
 let errorBox = document.getElementById("errorBox");
 let todoTaskForm = document.getElementById("todoTaskForm");
@@ -6,9 +6,11 @@ let todoShowBox = document.getElementById("todoShowBox");
 let dateInput = document.getElementById("taskDueDate");
 let upcomingContainer = document.getElementById("upcomingContainer");
 
+const searchInput = document.querySelector("[data-search]");
+
 let upComingTodoShowList = document.getElementById("upComingTodoShowList");
 let todaysTodoShowList = document.getElementById("todaysTodoShowList");
-let isOpen = true;
+let isOpen = false;
 
 const today = new Date().toISOString().split("T")[0];
 dateInput.setAttribute("min", today);
@@ -17,15 +19,22 @@ dateInput.defaultValue = today;
 reloadTodoList(todos);
 showTodaysTodos();
 
+const closeForm = () => {
+  todoTaskForm.style.display = "none";
+  todoShowBox.style.display = "flex";
+  isOpen = false;
+};
+
 function toggleTodoForm() {
-  if (isOpen) {
+  if (!isOpen) {
     todoTaskForm.style.display = "block";
     todoShowBox.style.display = "none";
+    isOpen = true;
   } else {
     todoTaskForm.style.display = "none";
     todoShowBox.style.display = "flex";
+    isOpen = false;
   }
-  isOpen = !isOpen;
 }
 
 function getTodoFormDetails(form) {
@@ -68,6 +77,21 @@ function convertISOToLongDate(ISODateString) {
   });
 }
 
+searchInput.addEventListener("input", (e) => {
+  const inputValue = e.target.value.toLowerCase();
+
+  todos.forEach((todo) => {
+    const task = todo.taskName.toLowerCase();
+    const dueDate = todo.convertedTaskDueDate.toLowerCase();
+
+    const isVisible = task.includes(inputValue) || dueDate.includes(inputValue);
+
+    if (todo.element) {
+      todo.element.classList.toggle("hide", !isVisible);
+    }
+  });
+});
+
 function reloadTodoList(todoDatas) {
   const todaysDate = convertISOToLongDate(today);
 
@@ -91,8 +115,15 @@ function reloadTodoList(todoDatas) {
           return;
         }
 
+        const elementID = `todo-${todo__id}`;
+
+        setTimeout(() => {
+          const element = document.getElementById(elementID);
+          if (element) todoDatas.element = element;
+        }, 0);
+
         return `
-          <div class="todoList">
+          <div class="todoList" id="${elementID}">
             <div class="todoIcon">
               <i class="bx bx-calendar-event"></i>
             </div>
@@ -103,7 +134,11 @@ function reloadTodoList(todoDatas) {
               <span>${todoDatas.convertedTaskDueDate}</span>
             </div>
             <div class="opButtons">
-              <i class="bx bx-checks" onclick="markAsDoneOrUndone(${todo__id})"></i>
+              ${
+                !todoDatas.isDone
+                  ? `<i class='bxr  bx-check-circle' onclick="markAsDoneOrUndone(${todo__id})" ></i>`
+                  : `<i class="bxr bxs-check-circle" onclick="markAsDoneOrUndone(${todo__id})"></i>`
+              }
               <i class="bx bx-trash" onclick="deleteTodo(${
                 todoDatas.uniqueID
               })"></i>
@@ -136,11 +171,15 @@ function addTodo(taskName, taskDescription, taskDueDate, taskPrioritization) {
     taskPrioritization,
     isDone: false,
   });
+
+  localStorage.setItem("pepcoTodos", JSON.stringify(todos, ...todos));
+
   reloadTodoList(todos);
   showTodaysTodos();
 
+  todoTaskForm.style.display = "none";
+  todoShowBox.style.display = "flex";
   isOpen = false;
-  toggleTodoForm();
 }
 
 function showTodaysTodos() {
@@ -166,8 +205,15 @@ function showTodaysTodos() {
     todaysTodoShowList.innerHTML = `
     ${todaysTodos
       .map((todoDatas, todo__id) => {
+        const elementID = `todo-${todo__id}`;
+
+        setTimeout(() => {
+          const element = document.getElementById(elementID);
+          if (element) todoDatas.element = element;
+        }, 0);
+
         return `
-          <div class="todoList">
+          <div class="todoList" id="${elementID}">
             <div class="todoIcon">
               <i class="bx bx-calendar-star isToday"></i>
             </div>
@@ -178,7 +224,11 @@ function showTodaysTodos() {
               <span>${todoDatas.convertedTaskDueDate}</span>
             </div>
             <div class="opButtons">
-              <i class="bx bx-checks" onclick="TodaysTodosMarkAsDoneOrUndone(${todo__id})"></i>
+              ${
+                !todoDatas.isDone
+                  ? `<i class='bxr  bx-check-circle' onclick="TodaysTodosMarkAsDoneOrUndone(${todo__id})" ></i>`
+                  : `<i class="bxr bxs-check-circle" onclick="TodaysTodosMarkAsDoneOrUndone(${todo__id})"></i>`
+              }
               <i class="bx bx-trash" onclick="deleteTodo(${
                 todoDatas.uniqueID
               })"></i>
@@ -194,11 +244,13 @@ function showTodaysTodos() {
 function TodaysTodosMarkAsDoneOrUndone(todo__id) {
   todaysTodos[todo__id].isDone = !todaysTodos[todo__id].isDone;
   showTodaysTodos();
+  localStorage.setItem("pepcoTodos", JSON.stringify(todos));
 }
 
 function markAsDoneOrUndone(todo__id) {
   todos[todo__id].isDone = !todos[todo__id].isDone;
   reloadTodoList(todos);
+  localStorage.setItem("pepcoTodos", JSON.stringify(todos));
 }
 
 function deleteTodo(todoUniqueID) {
@@ -207,6 +259,8 @@ function deleteTodo(todoUniqueID) {
     todos.splice(todoID, 1);
     reloadTodoList(todos);
     showTodaysTodos();
+
+    localStorage.setItem("pepcoTodos", JSON.stringify(todos));
   } else {
     alert("ID Not Found. Refresh and try again!.");
   }
