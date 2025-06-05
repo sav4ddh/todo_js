e0cd6729bef09080a00075b6bf9248f6a0b49f76
Muhammed Savaad <savaad.code@gmail.com>
2025-06-05 11:48:28 +0530

    Mark as done option added and delete function done

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const todos = [];
+let todaysTodos = [];
 let errorBox = document.getElementById("errorBox");
 let todoTaskForm = document.getElementById("todoTaskForm");
 let todoShowBox = document.getElementById("todoShowBox");
 let dateInput = document.getElementById("taskDueDate");
 
 let upComingTodoShowList = document.getElementById("upComingTodoShowList");
+let todaysTodoShowList = document.getElementById("todaysTodoShowList");
 let isOpen = true;
-
-reloadTodoList(todos);
 
 const today = new Date().toISOString().split("T")[0];
 dateInput.setAttribute("min", today);
 dateInput.defaultValue = today;
+
+reloadTodoList(todos);
+showTodaysTodos();
 
 function toggleTodoForm() {
   if (isOpen) {
@@ -65,36 +68,34 @@ function convertISOToLongDate(ISODateString) {
 }
 
 function reloadTodoList(todoDatas) {
+  const todaysDate = convertISOToLongDate(today);
+
   if (todoDatas.length === 0) {
-    upComingTodoShowList.innerHTML = `
-      <div class="todoShowList" id="todoShowList">
-        <div class="emptyTodoList">
-          <img src="/empty.svg" alt="empty trolley" width="200px" />
-          <h4>Empty todoList.</h4>
-          <span
-            >looks like you haven't added anything to your app. <br />
-            Go ahead and add new task.</span
-          >
-        </div>
-      </div>
-    `;
+    // Nothing here.
   } else {
     upComingTodoShowList.innerHTML = `
     ${todoDatas
-      .map((todoDatas, id) => {
-        console.log("Console from reloadTodoList: ", todoDatas);
+      .map((todoDatas, todo__id) => {
+        if (todoDatas.convertedTaskDueDate == todaysDate) {
+          return;
+        }
+
         return `
           <div class="todoList">
             <div class="todoIcon">
               <i class="bx bx-calendar-event"></i>
             </div>
-            <div class="todo">
+            <div class="todo ${
+              todoDatas.isDone ? "isDone" : ""
+            } id="todo-${todo__id}">
               <p>${todoDatas.taskName}</p>
               <span>${todoDatas.convertedTaskDueDate}</span>
             </div>
             <div class="opButtons">
-              <i class="bx bx-checks"></i>
-              <i class="bx bx-trash"></i>
+              <i class="bx bx-checks" onclick="markAsDoneOrUndone(${todo__id})"></i>
+              <i class="bx bx-trash" onclick="deleteTodo(${
+                todoDatas.uniqueID
+              })"></i>
             </div>
           </div>
         `;
@@ -104,25 +105,98 @@ function reloadTodoList(todoDatas) {
   }
 }
 
+function generateUniqueID(existingIDs) {
+  let newID;
+  do {
+    newID = Math.floor(100000 + Math.random() * 900000);
+  } while (existingIDs.includes(newID));
+  return newID;
+}
+
 function addTodo(taskName, taskDescription, taskDueDate, taskPrioritization) {
   let convertedTaskDueDate = convertISOToLongDate(taskDueDate);
+  let existingIDs = todos.map((todo) => todo.uniqueID);
+
   todos.push({
+    uniqueID: generateUniqueID(existingIDs),
     taskName,
     taskDescription,
     convertedTaskDueDate,
     taskPrioritization,
+    isDone: false,
   });
-  console.log(todos);
   reloadTodoList(todos);
+  showTodaysTodos();
 
   isOpen = false;
   toggleTodoForm();
 }
 
-// !  #TODO:
-// !  [+] #0: Remove append child fn and use innerHTML like react.
-// !  [+] #1: Redirect to todos page when user clicked "addTodo" button
-// !  [+] #2: Change the format of dueDate given by the user ie: "2025-06-25" to "25 June 2025"
-// !  [-] #3: Filter the dates into today and upcoming
-// !  [-] #4: Option to Mark As Done and Delete todo.
-// !  [-] #5: Option to search todos
+function showTodaysTodos() {
+  const todaysDate = convertISOToLongDate(today);
+  todaysTodos = todos.filter((todo) => {
+    return todo.convertedTaskDueDate == todaysDate;
+  });
+
+  if (todaysTodos.length === 0) {
+    todaysTodoShowList.innerHTML = `
+      <div class="todoShowList" id="todoShowList">
+        <div class="emptyTodoList">
+          <img src="/empty.svg" alt="empty trolley" width="200px" />
+          <h4>Empty todoList.</h4>
+          <span
+            >looks like you haven't added anything today. <br />
+            Go ahead and add new task.</span
+          >
+        </div>
+      </div>
+    `;
+  } else {
+    todaysTodoShowList.innerHTML = `
+    ${todaysTodos
+      .map((todoDatas, todo__id) => {
+        return `
+          <div class="todoList">
+            <div class="todoIcon">
+              <i class="bx bx-calendar-star isToday"></i>
+            </div>
+            <div class="todo ${
+              todoDatas.isDone ? "isDone" : ""
+            } id="todo-${todo__id}">
+              <p>${todoDatas.taskName}</p>
+              <span>${todoDatas.convertedTaskDueDate}</span>
+            </div>
+            <div class="opButtons">
+              <i class="bx bx-checks" onclick="TodaysTodosMarkAsDoneOrUndone(${todo__id})"></i>
+              <i class="bx bx-trash" onclick="deleteTodo(${
+                todoDatas.uniqueID
+              })"></i>
+            </div>
+          </div>
+        `;
+      })
+      .join("")}
+  `;
+  }
+}
+
+function TodaysTodosMarkAsDoneOrUndone(todo__id) {
+  todaysTodos[todo__id].isDone = !todaysTodos[todo__id].isDone;
+  showTodaysTodos();
+}
+
+function markAsDoneOrUndone(todo__id) {
+  todos[todo__id].isDone = !todos[todo__id].isDone;
+  reloadTodoList(todos);
+}
+
+function deleteTodo(todoUniqueID) {
+  const todoID = todos.findIndex((todo) => todo.uniqueID === todoUniqueID);
+  if (todoID !== -1) {
+    todos.splice(todoID, 1);
+    reloadTodoList(todos);
+    showTodaysTodos();
+  } else {
+    alert("ID Not Found. Refresh and try again!.");
+  }
+}
